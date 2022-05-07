8a52b7f170a9e175b586820f9cadbe5db31b52d2
yushicheng <1542874601@qq.com>
2022-05-07 23:30:58 +0800

    temp:临时节点标记 星期六 晚上 11:30:58 五月 7日 2022 修改了 decode.js 修改了 encode.js

--- encode.js
+++ encode.js
@@ -6,29 +6,30 @@ const { promisify } = require("util");
 const FragmentRecoder = require("./fragment_recoder");
 
 const encode_dir = path.resolve(__dirname, "./encode/");
-const input_file = path.resolve(__dirname, "./input/test2.mp4");
+const input_file = path.resolve(__dirname, "./input/20210312_1782415fad5_r1.mp4");
 
 /** 用户的唯一签名秘钥是一个uuid **/
 const secret_key = "f0cb9403-3766-4fea-9dc6-e129a1488c55";
-/** 暂且使用2048000的单位对文件进行切片 **/
-const unit_size = 20480000;
 
 async function encode() {
   /** 实例化一个过程记录者 **/
   const record = new FragmentRecoder();
-  record.unit_size = unit_size;
-  record.origin_file_path = input_file;
+  /** 读取文件 **/
   const resource_buffer = await promisify(fs.readFile)(input_file);
+  record.origin_file_path = input_file;
   record.origin_file_content = resource_buffer.buffer;
+  /** 计算切片大小 **/
+  const unit_size = 2 * parseInt(resource_buffer.length / 10);
+  record.unit_size = unit_size;
   /** 计算头部的二进制填充 **/
-  const header_buffer_pedding = Buffer.from(Array(unit_size).fill(0));
+  const header_buffer_pedding = new Uint8Array(Array(unit_size).fill(1));
   /** 文件尾需要填充的字符串 **/
-  const footer_buffer_pedding = Buffer.from(Array(Math.abs(unit_size - resource_buffer.length % unit_size)).fill(0));
+  const footer_buffer_pedding = new Uint8Array(Array(unit_size - resource_buffer.length % unit_size).fill(1));
   /** 获取到最终资源的内容 **/
   const concat_resourse_buffer = Buffer.concat([header_buffer_pedding, resource_buffer, footer_buffer_pedding]);
   /** 计算文件实际需要切割的份数 **/
   record.concat_content_size = concat_resourse_buffer.length;
-  const splite_block = parseInt(concat_resourse_buffer.length / unit_size);
+  const splite_block = concat_resourse_buffer.length / unit_size;
   record.pieces = splite_block;
   /** 执行切割 **/
   console.log(`实际需要切分成 ${splite_block} 份`);
@@ -37,7 +38,6 @@ async function encode() {
   while (current_count !== splite_block) {
     const every_origin_block = new Uint8Array(concat_resourse_buffer.buffer.slice(current_count * unit_size, unit_size * (current_count + 1)));
     const every_origin_content = every_origin_block.toString();
-    // console.log("every_origin_content.length", every_origin_content.length);
     const output_file_path = path.join(encode_dir, `${current_count + 1}.txt`);
     const encode_content = CryptoJS.AES.encrypt(every_origin_content, secret_key).toString();
     await promisify(fs.writeFile)(output_file_path, encode_content, "utf-8");
